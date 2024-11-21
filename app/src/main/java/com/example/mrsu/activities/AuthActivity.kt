ef4397479de706e95ee60b20d53711e3d74bb55e
aleksey_karasev <alexkarasar@gmail.com>
2024-11-21 07:48:21 +0300

    Уведомление о неправильном вводе при авторизации

--- app/src/main/java/com/example/mrsu/activities/AuthActivity.kt
+++ app/src/main/java/com/example/mrsu/activities/AuthActivity.kt
@@ -1,6 +1,7 @@
 package com.example.mrsu.activities
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,9 +9,7 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
 import com.example.mrsu.R
 import com.example.mrsu.databinding.ActivityAuthBinding
-import com.example.mrsu.databinding.ActivityMainBinding
 import com.example.mrsu.objects.RequestObj.getAccessTokenRequest
-import com.example.mrsu.objects.RequestObj.getUserInfo
 
 class AuthActivity : AppCompatActivity() {
 
@@ -21,8 +20,7 @@ class AuthActivity : AppCompatActivity() {
 
 
         val binding: ActivityAuthBinding = DataBindingUtil.setContentView(
-            this,
-            R.layout.activity_auth
+            this, R.layout.activity_auth
         );
 
         setContentView(binding.auth)
@@ -34,12 +32,19 @@ class AuthActivity : AppCompatActivity() {
         }
 
         binding.loginButton.setOnClickListener {
-            getAccessTokenRequest(
-                this, "https://p.mrsu.ru/OAuth/Token",
-                binding.emailInput.text.toString(),
-                binding.passwordInput.text.toString()
-            )
-            getUserInfo(this)
+            if (binding.emailInput.text.toString() == "" || !binding.emailInput.text.toString()
+                    .contains('@')
+            ) {
+                Toast.makeText(this, "Введите email", Toast.LENGTH_LONG).show()
+            } else if (binding.passwordInput.text.toString() == "") {
+                Toast.makeText(this, "Введите пароль", Toast.LENGTH_LONG).show()
+            } else {
+                getAccessTokenRequest(
+                    this, "https://p.mrsu.ru/OAuth/Token",
+                    binding.emailInput.text.toString(),
+                    binding.passwordInput.text.toString()
+                )
+            }
         }
 
     }
