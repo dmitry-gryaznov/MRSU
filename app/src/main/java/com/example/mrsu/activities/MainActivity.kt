b8f11ba3751e0ca2161e32486e71267ab7dd363e
aleksey_karasev <alexkarasar@gmail.com>
2024-11-28 14:04:38 +0300

    Более менее работает

--- app/src/main/java/com/example/mrsu/activities/MainActivity.kt
+++ app/src/main/java/com/example/mrsu/activities/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.mrsu.activities
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,7 +12,6 @@ import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
 import com.example.mrsu.R
 import com.example.mrsu.databinding.ActivityMainBinding
-import com.example.mrsu.dataclasses.User
 import com.example.mrsu.objects.RequestObj.getUser
 import com.example.mrsu.objects.RequestObj.getUserInfoRequest
 import com.example.mrsu.objects.RequestObj.isTokenValid
@@ -21,6 +21,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
+        Log.i("MainActivity", "onCreate()")
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
@@ -28,6 +30,9 @@ class MainActivity : AppCompatActivity() {
             onPause()
             val intent = Intent(this, AuthActivity::class.java)
             startActivity(intent)
+            Log.i("MainActivity", "finish()")
+            finish()
+            return
         }
 
     }
@@ -35,13 +40,15 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
 
+        Log.i("MainActivity", "onStart()")
+
         getUserInfoRequest(this)
         getUser(this)
 
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(
             this,
             R.layout.activity_main
-        );
+        )
 
         setContentView(binding.main)
 
@@ -56,12 +63,30 @@ class MainActivity : AppCompatActivity() {
         Glide.with(this)
             .load(tmp?.photo?.urlSmall)
             .into(binding.userPhoto)
-        binding.userId.text = tmp?.id.toString()
-        binding.userName.text = tmp?.userName.toString()
-        binding.userBirthDate.text = tmp?.birthDate.toString()
+        binding.userId.text = "ID: " + tmp?.id.toString().take(8)
+        binding.userName.text = "ФИО пользователя: " + tmp?.userName.toString()
+        binding.userBirthDate.text = "Дата рождения: " + tmp?.birthDate.toString().take(10)
+
+        binding.logoutButton.setOnClickListener {
+            Log.i("MainActivity", "ButtonLogout clicked")
+            logout()
+            val intent = Intent(this, AuthActivity::class.java)
+            startActivity(intent)
+            Log.i("MainActivity", "finish()")
+            finish()
+        }
+
         //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        this.getSharedPreferences("app_prefs", Context.MODE_PRIVATE).edit().putString("access_token", null).apply()
+        //this.getSharedPreferences("app_prefs", Context.MODE_PRIVATE).edit()
+        //    .putString("access_token", null).apply()
         //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    }
+
+    private fun logout() {
+        val sharedPref = getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
+        sharedPref.edit().clear().apply()
+
+        Log.i("MainActivity.Logout", "User log outed")
     }
 
 }
--- app/src/main/java/com/example/mrsu/objects/RequestObj.kt
+++ app/src/main/java/com/example/mrsu/objects/RequestObj.kt
@@ -2,6 +2,7 @@ package com.example.mrsu.objects
 
 import android.content.Context
 import android.util.Log
+
 import com.example.mrsu.dataclasses.AccessTokenMessage
 import com.example.mrsu.dataclasses.User
 import com.google.gson.Gson
@@ -10,23 +11,48 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
 import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 object RequestObj {
 
     private val client = OkHttpClient()
     private val gson = Gson()
 
+    fun isTokenValid(context: Context): Boolean {
+        Log.i("isTokenValid()", "Started")
+        val sharedPref = context.getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
+        val accessToken = sharedPref.getString("access_token", null)
+        val expiresAt = sharedPref.getLong("token_expires_at", 0)
+        val currentTime = System.currentTimeMillis() / 1000 // Текущее время в секундах
 
-    //Есть ли токен вообще
-    fun isTokenValid(context: Context): Boolean{
-        val token = getAccessToken(context)
-        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        return !token.isNullOrEmpty()//Сюда вписать функцию для проверки ttl
-        //!!!!!!!!!!!!
+        if (accessToken.isNullOrEmpty()) {
+            Log.w("isTokenValid()", "No token")
+            return false
+        }
+
+        if (expiresAt <= currentTime) {
+            Log.w("isTokenValid()", "Token dead. Time : $currentTime, Expired : $expiresAt")
+            return false
+        }
+
+        Log.i("isTokenValid()", "Token valid. Expires at $expiresAt")
+        return true
     }
 
     //Запросы к Бд
-    fun getAccessTokenRequest(context: Context, urlString: String, username: String, password: String) {
+
+    //Получить токен
+    fun getAccessTokenRequest(
+        context: Context,
+        urlString: String,
+        username: String,
+        password: String,
+        onSuccess: () -> Unit,
+        onFailure: (String) -> Unit
+    ) {
+        Log.i("getAccessTokenRequest()", "Started")
 
         val formBody = FormBody.Builder()
             .add("username", username)
@@ -41,29 +67,40 @@ object RequestObj {
             .post(formBody)
             .addHeader("Content-Type", "application/x-www-form-urlencoded")
             .build()
-
+        Log.i("ee", request.toString())
         client.newCall(request).enqueue(object : okhttp3.Callback {
 
             override fun onFailure(call: okhttp3.Call, e: IOException) {
-                Log.w("Request token failed", e.message.toString())
-
+                Log.w("getAccessTokenRequest()", e.message.toString())
+                onFailure("Ошибка сети: ${e.message}")
             }
 
             override fun onResponse(call: okhttp3.Call, response: Response) {
-
                 if (response.isSuccessful) {
-                    val accessTokenMessage = gson.fromJson(response.body?.string(), AccessTokenMessage::class.java)
-                    Log.i("Successful auth request", accessTokenMessage.toString())
-                    saveAccessToken(context, accessTokenMessage)
-                }
-                else {
-                    Log.i("Unsuccessful auth request", response.code.toString())
+                    val responseBody = response.body?.string()
+                    try {
+                        val accessTokenMessage =
+                            gson.fromJson(responseBody, AccessTokenMessage::class.java)
+                        saveAccessToken(context, accessTokenMessage)
+                        Log.w("getAccessTokenRequest()", "Successful request: $accessTokenMessage")
+
+                        onSuccess()
+                    } catch (e: Exception) {
+                        Log.w("getAccessTokenRequest()", "Parsing problem: " + e.message.toString())
+                        onFailure("Ошибка парсинга ответа: ${e.message}")
+                    }
+                } else {
+
+                    Log.w("getAccessTokenRequest()", "Server problem: " + { response.code })
+                    onFailure("Ошибка сервера: ${response.code}")
                 }
             }
         })
     }
 
+    //Получить информацию о пользователе
     fun getUserInfoRequest(context: Context) {
+        Log.i("getUserInfoRequest()", "Started")
         val bearerToken = getAccessToken(context)
 
         val request = Request.Builder()
@@ -75,70 +112,108 @@ object RequestObj {
 
         client.newCall(request).enqueue(object : okhttp3.Callback {
             override fun onFailure(call: okhttp3.Call, e: IOException) {
-                Log.w("Request user failed", e.message.toString())
+                Log.w("getUserInfoRequest()", "Request user failed: " + e.message.toString())
             }
 
             override fun onResponse(call: okhttp3.Call, response: Response) {
                 if (response.isSuccessful) {
                     val user = gson.fromJson(response.body?.string(), User::class.java)
-                    Log.i("Successful user request", user.toString())
+                    Log.i("getUserInfoRequest()", "Successful user request")
                     saveUser(context, user)
-                } else {
-                    Log.i("Unsuccessful user request", response.code.toString())
+                }
+                //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                else if (response.code == 401) {
+                    Log.w("getUserInfoRequest()", "Token dead")
+                }
+                //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                else {
+                    Log.w(
+                        "getUserInfoRequest(): ",
+                        "Unsuccessful user request" + response.code.toString()
+                    )
                 }
             }
         })
     }
 
-    fun getStudentTimeTableRequest(context: Context){
+    //Получить расписанние (андрей сделает свое)
+    fun getStudentTimeTable(context: Context) {
+        val formatter = SimpleDateFormat("yyyy-MM-dd'T'00:00:00", Locale.getDefault())
+        val currentDate = formatter.format(Date())
+        // URL с закодированной датой
+        val url = "https://papi.mrsu.ru/v1/StudentTimeTable?date=$currentDate"
 
+        // Получение токена (замените функцией для вашего приложения)
+        val accessToken = getAccessToken(context)
 
+        // Создание клиента
+        val client = OkHttpClient()
+
+        // Создание запроса
         val request = Request.Builder()
-            .url("https://papi.mrsu.ru/v1/StudentTimeTable?date=")
-            .post(FormBody.Builder().build())
-            .addHeader("Authorization", "Bearer "+ getAccessToken(context))
+            .url(url)
+            .get()
+            .addHeader("Authorization", "Bearer $accessToken")
             .addHeader("Content-Type", "application/json; charset=utf-8")
             .build()
 
+        // Выполнение запроса
         client.newCall(request).enqueue(object : okhttp3.Callback {
-
             override fun onFailure(call: okhttp3.Call, e: IOException) {
-                Log.w("Request TimeTable failed", e.message.toString())
-
+                // Обработка ошибки
+                Log.e("TimeTableRequest", "Request failed: ${e.message}")
             }
 
-            override fun onResponse(call: okhttp3.Call, response: Response) {
-
+            override fun onResponse(call: okhttp3.Call, response: okhttp3.Response) {
+                // Проверяем успешность ответа
                 if (response.isSuccessful) {
-                    val accessTokenMessage = gson.fromJson(response.body?.string(), AccessTokenMessage::class.java)
-                    Log.i("Successful TimeTable request", accessTokenMessage.toString())
-                    saveAccessToken(context, accessTokenMessage)
-                }
-                else {
-                    Log.i("Unsuccessful TimeTable request", response.code.toString())
+                    val responseBody = response.body?.string()
+                    Log.i("TimeTableRequest", "Response: $responseBody")
+                    // Обработайте JSON-ответ при необходимости
+                } else {
+                    Log.e(
+                        "TimeTableRequest",
+                        "Error: ${response.code}, Message: ${response.message}"
+                    )
                 }
             }
         })
     }
+
     //SharedPreferences
     private fun saveAccessToken(context: Context, accessTokenMessage: AccessTokenMessage) {
+
+        Log.i("saveAccessToken()", "Started")
+
         val sharedPref = context.getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
-        sharedPref.edit().putString("access_token", accessTokenMessage.accessToken).apply()
-        Log.i("SharedPreferences edited token", accessTokenMessage.accessToken)
+        // Текущее время в секундах
+        val currentTime = System.currentTimeMillis() / 1000
+        val expiresAt = currentTime + accessTokenMessage.expiresIn
+
+        sharedPref.edit()
+            .putString("access_token", accessTokenMessage.accessToken)
+            .putString("refresh_token", accessTokenMessage.refreshToken)
+            .putLong("token_expires_at", expiresAt)
+            .apply()
+        val expDate = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(Date(expiresAt * 1000))
+        Log.i("saveAccessToken()", "Token saved : $expDate")
     }
 
     private fun getAccessToken(context: Context): String? {
+        Log.i("getAccessToken()", "Started")
         val sharedPref = context.getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
         return sharedPref.getString("access_token", null)
     }
 
-    private fun saveUser(context: Context, user: User){
+    private fun saveUser(context: Context, user: User) {
+        Log.i("saveUser()", "Started")
         val sharedPref = context.getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
         sharedPref.edit().putString("user", gson.toJson(user)).apply()
-        Log.i("SharedPreferences edited user", gson.toJson(user))
+        Log.i("saveUser()", "User saved : " + user.toString())
     }
 
-    public fun getUser(context: Context): User? {
+    fun getUser(context: Context): User? {
+        Log.i("getUser()", "Started")
         val prefs = context.getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
         val userJson = prefs.getString("user", null)
 
